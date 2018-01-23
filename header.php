3390dad04b03c261ecdf6668d60c9d9e9bb9324c
Andrew Minion <andrew@andrewrminion.com>
2018-01-23 10:43:38 -0500

    style header

--- header.php
+++ header.php
@@ -28,22 +28,17 @@
         <div class="site-branding">
             <?php
             the_custom_logo();
-            if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php
-            endif;
-
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-            <?php
-            endif; ?>
+            if ( get_field( 'custom_header' ) !== '' && ! is_null( get_field( 'custom_header' ) ) ) {
+                $title = get_field( 'custom_header' );
+            } else {
+                $title = lcfirst( str_replace( ' ', '<wbr>', get_the_title() ) );
+            }
+            ?>
+            <h1 class="page-title">&lt;<?php echo $title ?>/&gt;</h1>
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'andrewrminion' ); ?></button>
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'andrewrminion' ); ?></button>
             <?php
                 wp_nav_menu( array(
                     'theme_location' => 'menu-1',
