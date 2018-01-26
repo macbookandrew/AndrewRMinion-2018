970e1c74dece3518073fd9f569952f588154f00e
Andrew Minion <andrew@andrewrminion.com>
2018-01-25 22:21:00 -0500

    bump version

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package AndrewRMinion_Design_2018
  */
 
-const ARMD_THEME_VERSION = '1.0.0';
+const ARMD_THEME_VERSION = '1.0.1';
 
 if ( ! function_exists( 'andrewrminion_setup' ) ) :
     /**
