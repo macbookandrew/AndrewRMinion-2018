66b9386cc3d2e9c8c13da55d9aa3d9773f503273
Andrew Minion <andrew@andrewrminion.com>
2018-01-26 10:53:27 -0500

    improve link underlines

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package AndrewRMinion_Design_2018
  */
 
-const ARMD_THEME_VERSION = '1.0.2';
+const ARMD_THEME_VERSION = '1.0.3';
 
 if ( ! function_exists( 'andrewrminion_setup' ) ) :
     /**
